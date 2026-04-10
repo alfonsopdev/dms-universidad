9d9bfd74eec253663562f444345382492db07af9
alfonsopdev <1572501785@undc.edu.pe>
2026-04-10 17:55:29 -0500

    Descripción breve de lo que hiciste carga dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia';
 import router from './router/index.js';
 import App from './App.vue';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.withCredentials = true;
